704d15dbf92c4ee2b9c3c5f6b34713199bd66009
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2017-06-11 19:47:41 +0530

    Add files via upload

---
+++ FileHandler2.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+class FileHandler2
+{
+    public ArrayList<String> getEachLine()throws Exception
+    {
+        Scanner ob=new Scanner(new File("bichi.kdr"));
+        ArrayList<String> a=new ArrayList<String>();
+        while(ob.hasNext())
+        {
+            String text=ob.useDelimiter("\n").next();
+            a.add(text);
+        }
+        return a;
+    }
+}
